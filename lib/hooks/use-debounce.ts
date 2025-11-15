d3ab5e0b7a3b5e9b9d41af2600d6054e484f667e
Mark Hope <mark.hope@asymmetric.pro>
2025-11-15 16:41:09 -0600

    Fix missing React import in useDebounce hook

--- lib/hooks/use-debounce.ts
+++ lib/hooks/use-debounce.ts
@@ -20,6 +20,8 @@
  * }, [debouncedQuery])
  * ```
  */
+import { useEffect, useState } from 'react'
+
 export function useDebounce<T>(value: T, delay: number = 300): T {
   const [debouncedValue, setDebouncedValue] = useState<T>(value)
 
